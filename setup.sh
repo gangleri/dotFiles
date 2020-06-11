8ad2dccae42c3209b38e95233a40fe7b0bc0c510
Alan <alan@gangleri.net>
2020-06-11 11:57:17 +0100

    remove duplicate git clone

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ which git ||  (echo "Install XCode cli tools first xcode-select --install" && ex
 
 # install oh-my-zsh, vundle, Homebrew, nvm
 curl -o- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
-git clone git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -o- https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
 
