fb34bcc8b81d7640358cb0e5896ca06eb88e8f9c
Alan <alan@gangleri.net>
2020-06-12 09:28:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ which git ||  (echo "Install XCode cli tools and accept the license xcode-select
 
 # install oh-my-zsh, vundle, Homebrew, nvm
 curl -o- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -o- https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # generate ssh keys
 for k in 'GitHub' 'GitLab'
